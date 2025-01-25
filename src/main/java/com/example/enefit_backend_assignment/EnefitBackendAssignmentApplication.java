c3b99d19ff6836d141d2dbf43e267529985ad670
Robert Efros <refrosjob@gmail.com>
2025-01-25 22:18:41 +0200

    init-01 generated a new spring boot app

---
+++ src/main/java/com/example/enefit_backend_assignment/EnefitBackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.enefit_backend_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnefitBackendAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnefitBackendAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/enefit_backend_assignment/EnefitBackendAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enefit_backend_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnefitBackendAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
